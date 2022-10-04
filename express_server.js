b5ead84b56d89b29d7ad797c8a6c2f4c73e8a222
colspen <spencerlewiscole@gmail.com>
2022-10-04 01:32:20 +0000

    more notes yet

--- express_server.js
+++ express_server.js
@@ -23,27 +23,31 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }; // this object passing to ejs templates
-  ////    render method responds to requests by sending template and object with data template needs
+  const templateVars = { urls: urlDatabase }; 
+                    // (loop through urls keys in index)
+
+  ////    render method responds to requests by sending template and object with data template needs -> obj is paaed to EJS templates
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-////    Express route parameters pass data from frontend to backend via request url 
+  ////    Express route parameters pass data from frontend to backend via request url
 app.get("/urls/:id", (req, res) => {
 
   ////    creating variable that stores objects key values from client GETs
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = 
+  { id: req.params.id, longURL: urlDatabase[req.params.id] };
 
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
   ////    pull last key (newest) ??better way??
-  lastKey = req.params.id; //params is allowing to access the keys or values within the url
-  const longURL = urlDatabase[lastKey];
+  const newKey = req.params.id;
+  //params is allowing access of keys or values within the url
+  const longURL = urlDatabase[newKey];
   ////    link new short ID to longURL
   res.redirect(longURL);
 });
@@ -53,10 +57,10 @@ app.get("/u/:id", (req, res) => {
 ////    POST Routes
 ////////////////////////////////////////////////
 
-//// ???? how is form submit assigned to { longURL: key? }
+  //// <form> submit assigned via action and method attributes
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  let tinyID = generateRandomString()
+  let tinyID = generateRandomString();
   urlDatabase[tinyID] = req.body.longURL;
   res.redirect(`/urls/${tinyID}`);
 });
@@ -70,16 +74,16 @@ app.listen(PORT, () => {
 ////    short-URL-Code function temp location
 ////////////////////////////////////////////////
 
-function generateRandomString() {
+const generateRandomString = () => {
   // 6 characters long
   // generate letters and integers combined together randomly
   let tinyCode = '';
   const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   const charLength = chars.length;
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     tinyCode += chars.charAt(Math.floor(Math.random() *
       charLength));
   }
   return tinyCode;
-}
-// console.log(urlDatabase);
+};
+console.log(generateRandomString());
