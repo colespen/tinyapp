6dbfa398173a12d2838a4bfdf94363caafc91dd8
colspen <spencerlewiscole@gmail.com>
2022-10-07 04:04:55 +0000

    finalized express_server.js code and linting

--- express_server.js
+++ express_server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cookieSession = require('cookie-session');
 const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
-const { getUserByEmail, generateRandomString, urlsForUser 
+const { getUserByEmail, generateRandomString, urlsForUser
 } = require('./helpers');
 
 const app = express();
-const PORT = 8080; // default port 8080
-////    tell Express app to use EJS as its templating engine ---->
+const PORT = 8080;
+
 app.set("view engine", "ejs");
 
 /////////////////////////////////////////////////////////
@@ -22,7 +22,7 @@ app.use(cookieSession({
 app.use(morgan('dev'));
 
 /////////////////////////////////////////////////////////
-////    In-memory Object 
+////    In-memory Object
 /////////////////////////////////////////////////////////
 
 const urlDatabase = {
@@ -71,8 +71,8 @@ app.get("/urls", (req, res) => {
 });
 
 
-app.get("/urls/new", (req, res) => { 
-  
+app.get("/urls/new", (req, res) => {
+
   const id = req.session.user_id;
   const user = users[id];
   const templateVars = {
@@ -86,8 +86,9 @@ app.get("/urls/new", (req, res) => {
 
 
 app.get("/urls/:id", (req, res) => {
-  const user_id = req.session.user_id;
-  const user = users[user_id];
+
+  const userId = req.session.user_id;
+  const user = users[userId];
 
   const urlID = req.params.id;
   const url = urlDatabase[urlID];
@@ -95,13 +96,12 @@ app.get("/urls/:id", (req, res) => {
   if (!url) {
     return res.send("ID does not exist!");
   }
-  // console.log("* * * * * * * * * * * * * * * ", {users, urlDatabase}, url);
   if (!user || url.userID !== user.id) {
     return res.send("You don't have permission to edit this.");
   }
   const templateVars = {
     urlId: urlID,
-    longURL: url.longURL, //(removed longURL)
+    longURL: url.longURL,
     user
   };
   res.render("urls_show", templateVars);
@@ -109,33 +109,33 @@ app.get("/urls/:id", (req, res) => {
 
 
 app.get("/u/:id", (req, res) => {
+
   const id = req.params.id;
-  //params is allowing access of keys or values within the url
-  // console.log("GET* * * * * * * * * * * * * * * *",urlDatabase);
   const url = urlDatabase[id].longURL;
 
-  if (!url) { // if !undefined  (direct lookup in obj's)
+  if (!url) {
     return res.send("URL not found.");
   }
-
-  ////    link new short ID to longURL
   res.redirect(url);
 });
 
-app.get("/register", (req, res) => {
-  const user_id = req.session.user_id;
-  const user = users[user_id];
 
-  if (user) { //if undefined
+app.get("/register", (req, res) => {
+
+  const userId = req.session.user_id;
+  const user = users[userId];
+
+  if (user) {
     return res.redirect("/urls");
   }
   res.render("urls_register", { user: null });
-  ////    pass unnamed obj with empty user property
 });
 
+
 app.get("/login", (req, res) => {
-  const user_id = req.session.user_id;
-  const user = users[user_id];
+
+  const userId = req.session.user_id;
+  const user = users[userId];
   if (user) {
     return res.redirect("/urls");
   }
@@ -148,31 +148,30 @@ app.get("/login", (req, res) => {
 ////    POST Routes
 /////////////////////////////////////////////////////////
 
-//// <form> submit assigned via action and method attributes
-app.post("/urls", (req, res) => { //*********** FIX !user redirect
-  // console.log('---------- Added:\n', req.body);
-  const user_id = req.session.user_id;
-  const user = users[user_id];
+
+app.post("/urls", (req, res) => {
+  
+  const userId = req.session.user_id;
+  const user = users[userId];
   const tinyID = generateRandomString();
 
-  //--> add new key: value to obj
-  urlDatabase[tinyID] = req.body; //removed.longURL
-  urlDatabase[tinyID].userID = user.id; //removed.longURL
-  // console.log("* * * * * * * * * * * * * *", urlDatabase);
+  urlDatabase[tinyID] = req.body;
+  urlDatabase[tinyID].userID = user.id;
+  
   if (!user) {
     return res.send("Must be a TinyApp user to use this!");
   }
   res.redirect(`/urls/${tinyID}`);
 });
 
-////   Removes URL resource (key:value pair) from obj with delete button
+
 app.post("/urls/:id/delete", (req, res) => {
-  // console.log("---------- Resource removed:\n", req.params);
+  
   const urlID = req.params.id;
   const url = urlDatabase[urlID];
 
-  const user_id = req.session.user_id;
-  const user = users[user_id];
+  const userId = req.session.user_id;
+  const user = users[userId];
 
   if (!url) {
     return res.send("ID does not exist!");
@@ -183,22 +182,21 @@ app.post("/urls/:id/delete", (req, res) => {
   if (url.userID !== user.id) {
     return res.send("You don't have permission to delete that.");
   }
-
   delete urlDatabase[urlID];
 
   res.redirect("/urls");
 });
 
-////    Edits existing URLs resource *
+
 app.post("/urls/:id", (req, res) => {
-  // console.log('POST* * * * * * * * * * * * * * * * * Edited:\n');
+ 
   const urlID = req.params.id;
   const url = urlDatabase[urlID];
 
   const newURL = req.body.longURLupdate;
 
-  const user_id = req.session.user_id;
-  const user = users[user_id];
+  const userId = req.session.user_id;
+  const user = users[userId];
 
   if (!url) {
     return res.send("ID does not exist!");
@@ -209,19 +207,16 @@ app.post("/urls/:id", (req, res) => {
   if (url.userID !== user.id) {
     return res.send("You don't have permission to do that.");
   }
-
   urlDatabase[urlID].longURL = newURL;
 
   res.redirect("/urls");
 });
 
-////    Sets username cookie 
-app.post("/login", (req, res) => {
-  // console.log('---------- User login:\n', req.body);
 
+app.post("/login", (req, res) => {
+ 
   const { email, password } = req.body;
   const userMatch = getUserByEmail(email, users);
-  // const userMatch = lookupUserByEmail(email);
 
   if (!email || !password) {
     return res.render("urls_403", { user: null });
@@ -229,42 +224,37 @@ app.post("/login", (req, res) => {
   if (!userMatch) {
     return res.render("urls_403", { user: null });
   }
-  const passMatch = //checking matching
+  const passMatch =
     bcrypt.compareSync(password, userMatch.password);
   if (!passMatch) {
     return res.render("urls_403", { user: null });
   }
   const id = userMatch.id;
 
-  req.session.user_id = id; //set cookie
+  req.session.user_id = id;
   res.redirect("/urls");
 });
 
-////    Clears username cookie
-app.post("/logout", (req, res) => {
-  // console.log('--------- User logout:');
 
-  const id = req.session.user_id;
-  // console.log(id);
-  req.session = null;;
+app.post("/logout", (req, res) => {
+
+  req.session = null;
   res.redirect("/");
 });
 
-////    Adds new user to object
+
 app.post("/register", (req, res) => {
-  // console.log('---------- User added:');
+ 
   const { email, password } = req.body;
   const genId = generateRandomString();
   const userMatch = getUserByEmail(email, users);
-  // const userMatch = lookupUserByEmail(email);
-
+ 
   if (!email || !password) {
     return res.render("urls_400", { user: null });
   }
   if (userMatch) {
     return res.render("urls_400", { user: null });
   }
-  // encrypt pass.
   const hashedPass = bcrypt.hashSync(password, 10);
 
   users[genId] = {
@@ -281,7 +271,3 @@ app.post("/register", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
-
